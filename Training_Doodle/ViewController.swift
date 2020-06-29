b51f954fb3706962b947d9d0b729166d2fd61ccb
ChenCheWei <gunattack1120@gmail.com>
2020-06-29 20:25:05 +0800

    Init

---
+++ Training_Doodle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Training_Doodle
+//
+//  Created by mmslab-mini on 2020/6/29.
+//  Copyright © 2020 mmslab-mini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
